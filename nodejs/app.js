1dbebe99447dd21056f4e4fd64a88e88e5a6aae3
cranium44 <adabeechaim@gmail.com>
2021-09-21 11:31:45 +0100

    init commit

---
+++ nodejs/app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/test", (req, res) => {
+    res.send({ message: "GET request to the homepage" });
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
