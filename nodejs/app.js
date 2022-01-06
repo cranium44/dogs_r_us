7474fa5248d0d7f077ec73cc104a3a74832a484a
cranium44 <adabeechaim@gmail.com>
2022-01-06 17:15:02 +0100

    made configuration changes

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/test", (req, res) => {
-    res.send({ message: "GET request to the homepage" });
+	res.send({ message: "GET request to the homepage" });
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
